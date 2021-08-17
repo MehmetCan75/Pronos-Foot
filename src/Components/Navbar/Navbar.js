ca454c1f29cfd1f6b9b6b5458f559d9e8c2e98c2
Mehmet <mehmetozmenfb@gmail.com>
2021-08-17 03:35:38 +0200

    link issue

--- src/Components/Navbar/Navbar.js
+++ src/Components/Navbar/Navbar.js
@@ -37,7 +37,7 @@ export default function Navbar() {
     <nav>
         {(toggleMenu || largeur > 500)  && (
      <ul className="liste">
-         <Link to="/">
+         <Link to="/Pronos-Foot">
          <li className="items">Accueil</li>
          </Link>
          <Link to="/classement">
