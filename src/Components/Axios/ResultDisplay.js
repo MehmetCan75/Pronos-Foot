1ac4e00387981877c4a35d4e93841855b1555c9c
Mehmet <mehmetozmenfb@gmail.com>
2021-10-26 20:19:03 +0200

    11

--- src/Components/Axios/ResultDisplay.js
+++ src/Components/Axios/ResultDisplay.js
@@ -6,16 +6,16 @@ export default function ResultDisplay(props) {
     <div>
         <h3 className="playerName">Nom : {props.name}</h3>
         <ul className="displayResult">
-            <li>Saint-Etienne - Angers: {props.afirstGame}</li>
-            <li>Nantes - Clermont Foot: {props.bsecondGame}</li>
-            <li>Lille - Brest : {props.cthirdGame}</li>
-            <li>Nice - Lyon : {props.dfourthGame}</li>
-            <li>Lens - Metz : {props.efifthGame}</li>
-            <li>Lorient - Bordeaux : {props.fsixthGame}</li>
-            <li>Reims - Troyes : {props.gseventhGame}</li>
-            <li>Rennes - Strasbourg : {props.heigthGame}</li>
-            <li>Monaco - Montpellier : {props.ininethGame}</li>
-            <li>Marseille - PSG : {props.jtenthGame}</li>
+            <li>PSG - Lille: {props.afirstGame}</li>
+            <li>Metz - Saint-Etienne: {props.bsecondGame}</li>
+            <li>Lyon - Lens : {props.cthirdGame}</li>
+            <li>Angers - Nice : {props.dfourthGame}</li>
+            <li>Bordeaux - Reims : {props.efifthGame}</li>
+            <li>Montpellier - Nantes : {props.fsixthGame}</li>
+            <li>Strasbourg - Lorient : {props.gseventhGame}</li>
+            <li>Troyes - Rennes : {props.heigthGame}</li>
+            <li>Brest - Monaco : {props.ininethGame}</li>
+            <li>Clermont Foot - Marseille : {props.jtenthGame}</li>
             </ul>
     </div>
   )
