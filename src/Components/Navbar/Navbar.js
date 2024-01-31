feff882d7a57d1ca28aebbfa0781389de30c3613
Mehmet Can <can@Air-de-Mehmet.lan>
2024-01-31 18:03:26 +0100

    test

--- src/Components/Navbar/Navbar.js
+++ src/Components/Navbar/Navbar.js
@@ -40,7 +40,7 @@ export default function Navbar() {
     <nav>
         {(toggleMenu || largeur > 500)  && (
      <ul className="liste">
-         <Link to="/Pronos-Foot" onClick={closeMenu}>
+         <Link to="/NewHomePage" onClick={closeMenu}>
          <li className="items">Accueil</li>
          </Link>
          <Link to="/classement" onClick={closeMenu}>
